68ec3aa5b7027bc7538ed5bb5e41b525ff0669b1
Sam Cantrell <samuel@samuelcantrell.com>
2022-07-06 23:22:43 -0500

    Refactor some vars in ImageView

--- src/ImageView.cpp
+++ src/ImageView.cpp
@@ -15,18 +15,23 @@ void ImageView::write(ConwayLife& board)
     // ppm file header - P3 for 0-255 RGB as a text file
     outfile << "P3" << '\n';
     // image dimensions - width X height
-    int imagewidth(board.SizeX());
-    int imageheight(board.SizeY());
+    int image_width(board.SizeX());
+    int image_height(board.SizeY());
+    outfile << image_width << " " << image_height << '\n';
 
-    outfile << imagewidth << " " << imageheight << '\n';
-    outfile << "255" << '\n';
+    int max_rgb_val(255);
+    outfile << max_rgb_val << '\n';
 
     // image data -- each pixel in image is an RGB triplet.
     // can have line breaks or just spaces in between each
     // triplet
-    for (int y = 0; y < imageheight; y++)
+
+    // note that I tested code where I did one big loop rather than
+    // two nested loops but the time savings were minimal so I felt
+    // the clearer code was the better choice
+    for (int y = 0; y < image_height; y++)
     {
-        for (int x = 0; x < imagewidth; x++)
+        for (int x = 0; x < image_width; x++)
         {
             outfile << CellString(board.IsAlive(x, y)) << '\n';
         }
