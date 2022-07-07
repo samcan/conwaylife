ce74e4cf537e6d15fa1f41dda39c23db3bc3ad58
Sam Cantrell <samuel@samuelcantrell.com>
2022-07-06 21:47:10 -0500

    Rearrange public above private in ConwayLife class

--- src/ConwayLife.h
+++ src/ConwayLife.h
@@ -4,6 +4,22 @@
 #include <vector>
 
 class ConwayLife {
+public:
+    ConwayLife(const int x, const int y);
+    ~ConwayLife();
+
+    auto SizeX() const -> int { return m_size_x; }
+    auto SizeY() const -> int { return m_size_y; }
+    auto GenerationNum() const -> int { return m_generation; }
+
+    bool IsAlive(const int x, const int y) const;
+    void SetAlive(const int x, const int y);
+    void SetDead(const int x, const int y);
+    void CalcNextGeneration();
+
+    // instead of using a 2D array we're using a 1D array and indexing
+    // into the appropriate element
+    std::vector<bool> m_board;
 private:
     int m_size_x;
     int m_size_y;
@@ -13,25 +29,6 @@ private:
     int CountAliveNeighbors(const int x, const int y) const;
 
     inline int Index(const int x, const int y) const;
-public:
-    ConwayLife(const int x, const int y);
-    ~ConwayLife();
-
-    bool IsAlive(const int x, const int y) const;
-    void SetAlive(const int x, const int y);
-    void SetDead(const int x, const int y);
-
-    auto GenerationNum() const -> int { return m_generation; }
-
-    void CalcNextGeneration();
-
-    auto SizeX() const -> int { return m_size_x; }
-    auto SizeY() const -> int { return m_size_y; }
-
-
-    // instead of using a 2D array we're using a 1D array and indexing
-    // into the appropriate element
-    std::vector<bool> m_board;
 };
 
 
